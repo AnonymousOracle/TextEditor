38b96457fc158b3ad61b2c715b658fb8875575b2
Rostislav Tolushkin <rostislavtoluskinx@gmail.com>
2019-03-19 19:15:47 +0300

    In MainWindow.java: 	Prototype of a line counter was added (must be reworked); 	Correct layout had been realised;

--- src/mainwindow/MainWindow.java
+++ src/mainwindow/MainWindow.java
@@ -1,8 +1,13 @@
 package mainwindow;
 
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -10,12 +15,14 @@ import javax.swing.JLabel;
 import javax.swing.JMenuBar;
 import javax.swing.JMenu;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.JTextArea;
-
 
 public class MainWindow{
 	public JFrame frame;
 
+	private int amountOfStrings = 1;
+	
 	public MainWindow() {
 		initialize();
 	}
@@ -27,15 +34,30 @@ public class MainWindow{
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocationRelativeTo(null);
 
-		//JPanel mainPanel = new JPanel(new FlowLayout());
-		JTextArea mainTextArea = new JTextArea();
+		JPanel mainPanel = new JPanel(new BorderLayout());
+		JPanel editorPanel = new JPanel(new FlowLayout());
 		
-		// TODO: Organize correct layout
 		JLabel lineNumbers = new JLabel();
-		lineNumbers.setText("1");
+
+		lineNumbers.setText(String.valueOf(amountOfStrings));
 		
-		frame.add(lineNumbers);
-		frame.add(mainTextArea);
+		int rows = 1;
+		int columns = frame.getWidth() / 5;
+		JTextArea mainTextArea = new JTextArea(rows, columns);
+//		mainTextArea.addKeyListener(new KeyAdapter() {
+//			public void keyPressed(KeyEvent keyEvent) {
+//				if (keyEvent.getKeyCode() == KeyEvent.VK_ENTER) {
+//					amountOfStrings++;
+//					lineNumbers.setText(lineNumbers.getText() + "\n" + String.valueOf(amountOfStrings));
+//				}
+//			}
+//		});
+		
+		editorPanel.add(lineNumbers);
+		editorPanel.add(mainTextArea);
+		
+		mainPanel.add(editorPanel, BorderLayout.WEST);
+		frame.add(mainPanel);
 
 		createMainMenuBar();
 	}
